3744d0ba69a5012a5bf9f61ccdd4a9b7e1d617b7
Kurt Landrus <klandrus@gmail.com>
2015-08-19 12:45:00 -0400

    home controller spec passes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,17 +6,16 @@ class HomeController < ApplicationController
   def check
     val = params[:val].to_i
 
-    result = case val
+    result = case
+    when ((val % 3 ==0) and (val % 5 ==0))
+      'FizzBuzz'
     when val % 3 == 0
       'Fizz'
     when val % 5 == 0
       'Buzz'
-    when (!(val % 3) and !(val % 5))
-      'FizzBuzz'
     else
       ""
     end
-    binding.pry
     render json: result
   end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -9,10 +9,32 @@ RSpec.describe HomeController, type: :controller do
     end
   end
 
-  describe "GET #check" do
-    it "returns http success" do
-      get :check
-      expect(response).to have_http_status(:success)
+  describe "POST #check" do
+    it "returns empty string" do
+      post :check, val: 1
+      expect(response.body).to eql ""
+    end
+
+    context "with valid attributes" do
+      it "returns Fizz" do
+        post :check, val: 3
+        expect(response.body).to eql 'Fizz'
+      end
+
+      it "returns Buzz" do
+        post :check, val: 5
+        expect(response.body).to eql 'Buzz'
+      end
+
+      it "returns FizzBuzz" do
+        post :check, val: 15
+        expect(response.body).to eql 'FizzBuzz'
+      end
+
+    end
+
+    context "with in_valid attributes" do
+
     end
   end
 
